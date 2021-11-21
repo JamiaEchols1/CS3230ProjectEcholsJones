cbdfb7e522b56523dfa1be2765c42677669d772d
Jamia Echols <jechols5@my.westga.edu>
2021-11-20 20:18:07 -0500

    Added open query interface button

--- src/edu/westga/dbaccess/dal/AdminQueryInterfaceDAL.java
+++ src/edu/westga/dbaccess/dal/AdminQueryInterfaceDAL.java
@@ -1,5 +1,38 @@
 package edu.westga.dbaccess.dal;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
+/**
+ * The admin Query Interface DAL
+ * 
+ * @author Jamia Echols
+ * @Version Fall 2021
+ *
+ */
 public class AdminQueryInterfaceDAL {
+
+	public String runQuery(String query) throws SQLException {
+		String results = "";
+		try (Connection connection = DriverManager.getConnection(ConnectionString.CONNECTION_STRING);
+				PreparedStatement stmt = connection.prepareStatement(query)) {
+
+			ResultSet rs = stmt.executeQuery();
+			ResultSetMetaData rsmd = rs.getMetaData();
+			int numberOfColumns = rsmd.getColumnCount();
+			while (rs.next()) {
+				for (int i = 1; i <= numberOfColumns; i++) {
+					results += rsmd.getColumnName(i) + ": " + rs.getObject(i) + " |";
+				}
+				results += System.lineSeparator();
+			}
+
+		}
+		return results;
+	}
 
 }
--- src/edu/westga/dbaccess/view/AdminQueryWindowCodeBehind.java
+++ src/edu/westga/dbaccess/view/AdminQueryWindowCodeBehind.java
@@ -1,5 +1,60 @@
 package edu.westga.dbaccess.view;
+import java.sql.SQLException;
+
+import edu.westga.dbaccess.dal.AdminQueryInterfaceDAL;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class AdminQueryWindowCodeBehind {
 
+    @FXML
+    private TextField queryTextField;
+
+    @FXML
+    private Button submitButton;
+
+    @FXML
+    private Button clearButton;
+
+    @FXML
+    private TextArea resultsTextArea;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private Label adminInformaationLabel;
+    
+    private AdminQueryInterfaceDAL adminQueryInterfacedal;
+    
+    public AdminQueryWindowCodeBehind() {
+    	this.adminQueryInterfacedal = new AdminQueryInterfaceDAL();
+    }
+
+    @FXML
+    void handleBackButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleClearButtonCLick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSubmitButton(ActionEvent event) {
+    	try {
+    		this.resultsTextArea.setText(this.adminQueryInterfacedal.runQuery(this.queryTextField.getText()));
+    	} catch (SQLException exception) {
+    		Alert alert = new Alert(AlertType.ERROR);
+    		alert.setContentText(exception.getMessage());
+    		alert.show();
+    	}
+    }
 }
